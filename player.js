44b00b1f0b8f9b41ac1ddc60abaecbcbdade1a58
priyavema <53120689+priyavema@users.noreply.github.com>
2019-07-21 01:37:04 +0530

    Update player.js
    adding content to have continuous tooltip visibility

--- player.js
+++ player.js
@@ -1,21 +1,33 @@
 $(function(){
-    
+
          $.ajax({
              type:'GET',
              url: 'simple.json',
              dataType:'JSON',
+             
              success:function(data){
+               
                  $.each(data.steps, function(key, val){
-                    console.log(val.selector);
-                    $(val.selector+ '> .tooltiptext').text(val.content);
-                   
-                   
-                 })
+                  
+                     $(val.selector+ '> .tooltiptext').text(val.content);
+                      
+                  });
+                 callTooltip(data);
              },
              error:function(){
                                  
              }
+            
          });
-         
+
+        function callTooltip(result){
+         for(var i =0;i < result.steps.length;i++)
+            {
+                
+            var item = result.steps[i];
+            console.log(item.content);
+            
+            }
+        }    
          
          });
